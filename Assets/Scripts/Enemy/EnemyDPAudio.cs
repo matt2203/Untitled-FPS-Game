5c99f38a32ee44cbf5e53495576c8d6745a7d149
MH <matthewlukehenson@outlook.com>
2026-06-15 14:00:52 +0100

    Update

---
+++ Assets/Scripts/Enemy/EnemyDPAudio.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class EnemyDPAudio : MonoBehaviour
+{
+    public GameObject Audio;
+    public GameObject AudioSpawn;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Ground")
+        {
+            Instantiate(Audio, AudioSpawn.transform.position, AudioSpawn.transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+
+}
--- Assets/Scripts/Player/PlayerDPAudio.cs
+++ Assets/Scripts/Player/PlayerDPAudio.cs
@@ -1,20 +1,13 @@
 using UnityEngine;
 
-public class DPAudio : MonoBehaviour
+public class PlayerDPAudio : MonoBehaviour
 {
     public GameObject Audio;
     public GameObject AudioSpawn;
-    public GameObject AudioSpawn2;
-
-    void OnTriggerEnter(Collider other)
-    {
-        Instantiate(Audio, AudioSpawn2.transform.position, AudioSpawn2.transform.rotation);
-    }
 
     void OnCollisionEnter(Collision collision)
     {
         Instantiate(Audio, AudioSpawn.transform.position, AudioSpawn.transform.rotation);
+        AudioSpawn.SetActive(false);
     }
-
-
-}  
+}
