07cedf06469045624c5fb7acc6890755cd55f2a3
MH <matthewlukehenson@outlook.com>
2026-06-17 14:07:37 +0100

    .

--- Assets/Scripts/Player/PlayerDPAudio.cs
+++ Assets/Scripts/Player/PlayerDPAudio.cs
@@ -4,10 +4,12 @@ public class PlayerDPAudio : MonoBehaviour
 {
     public GameObject Audio;
     public GameObject AudioSpawn;
+    public GameObject AudioSource;
 
     void OnCollisionEnter(Collision collision)
     {
         Instantiate(Audio, AudioSpawn.transform.position, AudioSpawn.transform.rotation);
         AudioSpawn.SetActive(false);
+        AudioSource.SetActive(false);
     }
 }
